d922dc7f452decfc6ff3809feadcc4a54effd171
Denis Bittante <denisbittante@gmail.com>
2017-11-19 12:33:56 +0100

    clean code

--- app/service/src/main/java/io/swagger/api/impl/GeneratepdfApiServiceImpl.java
+++ app/service/src/main/java/io/swagger/api/impl/GeneratepdfApiServiceImpl.java
@@ -1,20 +1,12 @@
 package io.swagger.api.impl;
 
-import io.swagger.api.*;
-import io.swagger.model.*;
-
-import io.swagger.model.Pdf;
-
-import java.util.List;
-import io.swagger.api.NotFoundException;
-
-import java.io.InputStream;
-
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
-import javax.validation.constraints.*;
+
+import io.swagger.api.ApiResponseMessage;
+import io.swagger.api.GeneratepdfApiService;
+import io.swagger.api.NotFoundException;
+import io.swagger.model.Pdf;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2017-11-04T08:37:53.279Z")
 public class GeneratepdfApiServiceImpl extends GeneratepdfApiService {
     @Override
