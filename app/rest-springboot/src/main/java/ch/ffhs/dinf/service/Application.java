6de50574e1e630a8e97008ad614b3b498f4d73a7
Denis Bittante <denisbittante@gmail.com>
2018-01-22 18:35:14 +0100

    heroku deployable

--- app/rest-springboot/src/main/java/ch/ffhs/dinf/service/Application.java
+++ app/rest-springboot/src/main/java/ch/ffhs/dinf/service/Application.java
@@ -2,9 +2,19 @@ package ch.ffhs.dinf.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication(scanBasePackages = { "ch.ffhs.dinf.service", "ch.ffhs.dinf.osre" })
 public class Application {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- app/rest-springboot/src/main/java/ch/ffhs/dinf/service/PdfServiceController.java
+++ app/rest-springboot/src/main/java/ch/ffhs/dinf/service/PdfServiceController.java
@@ -1,8 +1,10 @@
 package ch.ffhs.dinf.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.ffhs.dinf.osre.engine.PdfEngine;
@@ -18,21 +20,20 @@ public class PdfServiceController {
 	private PdfEngine engine;
 
 	@RequestMapping(method = RequestMethod.POST, path = "scenario1")
-	public PdfResponse scenario1(PdfRequestScenario1 req) {
+	public @ResponseBody PdfResponse scenario1(@RequestBody PdfRequestScenario1 req) {
 
 		return engine.createPdfSzenario1(req);
 
 	}
 
 	@RequestMapping(method = RequestMethod.POST, path = "scenario2")
-	public PdfResponse scenario2(PdfRequestScenario2 req) {
+	public @ResponseBody PdfResponse scenario2(@RequestBody PdfRequestScenario2 req) {
 
 		return engine.createPdfSzenario2(req);
-
 	}
 
 	@RequestMapping(method = RequestMethod.POST, path = "scenario3")
-	public PdfResponse scenario3(PdfRequestScenario3 req) {
+	public @ResponseBody PdfResponse scenario3(@RequestBody PdfRequestScenario3 req) {
 
 		return engine.createPdfSzenario3(req);
 
